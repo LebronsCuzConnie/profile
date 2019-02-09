e9dfe99d613d7ce1e1058ba97779bc46454eb1a3
LeBronsCuzConnie <constance.rosemond@csedge.org>
2019-02-09 12:50:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let newHeading = document.createElement("Wassup");
-newHeading.innerHTML = "FUN!";
 let about = document.getElementById("about");
-about.appendChild(newHeading);
+let bodyArray = document.getElementsByTagName("BODY");
+let body = bodyArray[0];
+
+document.addEventListener("click", whenClicked);
+
+about.addEventListener("mouseover", whenHovered);
+about.addEventListener("mouseout", whenDoneHovering);
+
+function whenClicked(){
+	let newHeading = document.createElement("h1");
+	newHeading.innerHTML = "FUN";
+	newHeading.style.color = "blue";
+	newHeading.style.fontSize = "48px";
+	let about = document.getElementById("about");
+	about.appendChild(newHeading);
+}
+
+function whenHovered(){
+	body.style.color = "pink";
+}
+
+function whenDoneHovering(){
+	body.style.color = "white";
+}
